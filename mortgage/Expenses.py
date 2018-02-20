040f3b7842476e371c485d82e91c68ee717f9576
Dmitry Makovey <dmakovey@yahoo.com>
2018-02-19 23:36:36 -0800

    timestamp expenses

--- mortgage/Expenses.py
+++ mortgage/Expenses.py
@@ -2,9 +2,10 @@
 
 class Expense(object):
     """Represent expenses during various stages of mortgage process"""
-    def __init__(self, title, cost):
+    def __init__(self, title, cost, expense_date):
         self._title=title
         self._cost=cost
+        self._date=expense_date
 
     def __str__(self):
         return "{0}: {1:.2f}".format(self._title, self._cost)
@@ -17,10 +18,17 @@ class Expense(object):
     def cost(self):
         return self._cost
 
+    @property
+    def date(self):
+        return self._date
+
 class ExpenseList(object):
     def __init__(self,**kwargs):
         self._expenses=[]
-        self._options=kwargs
+        if kwargs:
+            self._options=kwargs
+        else:
+            self._options={}
     
     def addExpense(self, expense):
         self._expenses.append(expense)
@@ -28,7 +36,18 @@ class ExpenseList(object):
     def sum(self):
         return sum([e.cost for e in self._expenses])
 
-    @property(doc="Options common for this list")
+    def __str__(self):
+        return "{0}\nTotal: {1:.2f}".format(str([("{0} ({1})".format(e.title, e.date), e.cost) for e in self._expenses]),self.sum())
+
+    @property
     def options(self):
         """return expenseList options"""
         return self._options
+
+
+if __name__ == '__main__':
+    import datetime
+    el=ExpenseList()
+    el.addExpense(Expense('closing cost',10000,datetime.date(2009,11,2)))
+    el.addExpense(Expense('Title Insurance',1000,datetime.date(2009,11,4)))
+    print(el)
