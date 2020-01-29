a0f593ded990bcca2be798d20d325248815dae5a
Dmitry Makovey <dmakovey@yahoo.com>
2020-01-28 23:14:51 -0700

    bump version for python3 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='pymortgage',
-    version='0.1.4',
+    version='0.1.5',
     author="Dmytro Makovey",
     author_email="dmakovey@yahoo.com",
     description=("Python Mortgage utilities"),
